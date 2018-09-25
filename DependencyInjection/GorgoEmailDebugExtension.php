47602e3c051264958b86944fe4297514de42de5a
Nikita Makarov <mesaverde228@gmail.com>
2018-09-25 15:59:56 +0300

    New Features

---
+++ DependencyInjection/GorgoEmailDebugExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gorgo\Bundle\EmailDebugBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class GorgoEmailDebugExtension extends Extension
+{
+    const ALIAS = 'gorgo_email_debug';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAlias()
+    {
+        return self::ALIAS;
+    }
+}
--- GorgoEmailDebugBundle.php
+++ GorgoEmailDebugBundle.php
@@ -2,9 +2,16 @@
 
 namespace Gorgo\Bundle\EmailDebugBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GorgoEmailDebugBundle extends Bundle
 {
-
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
